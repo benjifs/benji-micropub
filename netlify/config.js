ef932fd4cc7cdb2fc8825339ab23ce763f682b91
Benji Encalada Mora <benjifs@users.noreply.github.com>
2025-08-11 01:18:37 -0500

    feat: configure for my env

--- netlify/config.js
+++ netlify/config.js
@@ -19,40 +19,42 @@ export const micropub = new Micropub({
 	store,
 	me: ME,
 	tokenEndpoint: TOKEN_ENDPOINT,
-	// contentDir: 'src',
+	contentDir: 'src/content',
 	// mediaDir: 'uploads',
 	// https://micropub.spec.indieweb.org/#configuration
 	config: {
-		// 'media-endpoint': 'https://micropub.example.com/media',
-		// 'syndicate-to': [
-		// 	{ uid: 'https://fed.brid.gy/', name: 'w/ Bridgy Fed', checked: true },
-		// ],
-		// 'post-types': [
-		// 	{ type: 'note', name: 'Note' },
-		// 	{ type: 'photo', name: 'Photo' },
-		// 	{ type: 'reply', name: 'Reply' },
-		// 	{ type: 'bookmark', name: 'Bookmark' },
-		// 	{ type: 'like', name: 'Like' },
-		// 	{ type: 'article', name: 'Article' },
-		// 	{ type: 'rsvp', name: 'RSVP' },
-		// 	{ type: 'repost', name: 'Repost' },
-		// 	{ type: 'watch', name: 'Watch' },
-		// 	{ type: 'read', name: 'Read' },
-		// 	{ type: 'listen', name: 'Listen' },
-		// 	{ type: 'game', name: 'Game' },
-		// ],
+		'media-endpoint': 'https://micropub.benji.dog/media',
+		'syndicate-to': [
+			{ uid: 'https://fed.brid.gy/', name: 'w/ Bridgy Fed', checked: true },
+			{ uid: 'https://brid.gy/publish/bluesky', name: 'to Bluesky' },
+			{ uid: 'https://brid.gy/publish/mastodon', name: 'to benji@corteximplant.com' }
+		],
+		'post-types': [
+			{ type: 'note', name: 'Note' },
+			{ type: 'photo', name: 'Photo' },
+			{ type: 'reply', name: 'Reply' },
+			{ type: 'bookmark', name: 'Bookmark' },
+			{ type: 'like', name: 'Like' },
+			{ type: 'article', name: 'Article' },
+			{ type: 'rsvp', name: 'RSVP' },
+			// { type: 'repost', name: 'Repost' },
+			{ type: 'watch', name: 'Watch' },
+			{ type: 'read', name: 'Read' },
+			{ type: 'listen', name: 'Listen' },
+			{ type: 'game', name: 'Game' },
+		],
 	},
-	// formatSlug: (type, filename) => {
-	// 	const typeToSlug = {
-	// 		like: 'likes',
-	// 		bookmark: 'bookmarks',
-	// 		rsvp: 'rsvp',
-	// 		article: 'articles',
-	// 		watch: 'watched',
-	// 		read: 'read',
-	// 		listen: 'listen',
-	// 		play: 'play'
-	// 	}
-	// 	return `${typeToSlug[type] || 'notes'}/${filename}`
-	// },
+	formatSlug: (type, filename) => {
+		const typeToSlug = {
+			like: 'likes',
+			bookmark: 'bookmarks',
+			rsvp: 'rsvp',
+			article: 'articles',
+			watch: 'watched',
+			read: 'read',
+			listen: 'listen',
+			play: 'play',
+		}
+		return `${typeToSlug[type] || 'notes'}/${filename}`
+	},
 })
